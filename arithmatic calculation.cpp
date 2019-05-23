36f55ac431801acbd5a7c993f61d5b1b90560920
Sazzad-Saju <sazzadsaju17@gmail.com>
2019-05-24 02:14:45 +0600

    Manipulation edited to correct

--- arithmatic calculation.cpp
+++ arithmatic calculation.cpp
@@ -11,9 +11,9 @@ int main()
 	cout<<setw(12)<<"Number1"<<setw(12)<<"Number2"<<endl;
 	cout<<setw(12)<<a<<setw(12)<<b<<endl<<endl;
 	cout<<setw(12)<<"--------------------------------"<<endl;
-	cout<<setw(16)<<"Addition: "<<setw(16)<<a+b<<endl;
-	cout<<setw(16)<<"Substraction: "<<setw(16)<<a-b<<endl;
-	cout<<setw(16)<<"Multiplication: "<<setw(16)<<a*b<<endl;
-	cout<<setw(16)<<"Division: "<<setw(16)<<a/b<<endl;
+	cout<<setw(26)<<"Addition: "<<setw(9)<<a+b<<endl;
+	cout<<setw(30)<<"Substraction: "<<setw(5)<<a-b<<endl;
+	cout<<setw(32)<<"Multiplication: "<<setw(3)<<a*b<<endl;
+	cout<<setw(26)<<"Division: "<<setw(9)<<a/b<<endl;
 	return 0;
 }
