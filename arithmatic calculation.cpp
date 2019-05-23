3d55e19709ddb364d91ed50860de255e97bb10ff
Sazzad-Saju <sazzadsaju17@gmail.com>
2019-05-24 01:37:32 +0600

    arithmatic calculation core code added

---
+++ arithmatic calculation.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter two integer number: ";
+	cin>>a>>b;
+	cout<<"Addition: "<<a+b<<endl;
+	cout<<"Substraction: "<<a-b<<endl;
+	cout<<"Multiplication: "<<a*b<<endl;
+	cout<<"Division: "<<a/b<<endl;
+	return 0;
+}
