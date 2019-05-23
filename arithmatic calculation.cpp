cc489732119d94271862114a387d068ba4a6eaf3
Sazzad-Saju <sazzadsaju17@gmail.com>
2019-05-24 01:51:10 +0600

    manipulation added

--- arithmatic calculation.cpp
+++ arithmatic calculation.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
+#include<iomanip>
+#include<stdlib.h>
 using namespace std;
 int main()
 {
 	int a,b;
 	cout<<"Enter two integer number: ";
 	cin>>a>>b;
-	cout<<"Addition: "<<a+b<<endl;
-	cout<<"Substraction: "<<a-b<<endl;
-	cout<<"Multiplication: "<<a*b<<endl;
-	cout<<"Division: "<<a/b<<endl;
+	system("cls");
+	cout<<setw(12)<<"Number1"<<setw(12)<<"Number2"<<endl;
+	cout<<setw(12)<<a<<setw(12)<<b<<endl<<endl;
+	cout<<setw(12)<<"--------------------------------"<<endl;
+	cout<<setw(16)<<"Addition: "<<setw(16)<<a+b<<endl;
+	cout<<setw(16)<<"Substraction: "<<setw(16)<<a-b<<endl;
+	cout<<setw(16)<<"Multiplication: "<<setw(16)<<a*b<<endl;
+	cout<<setw(16)<<"Division: "<<setw(16)<<a/b<<endl;
 	return 0;
 }
