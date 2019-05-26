3a3f9b25522b7072bdeb31731bdcfe75d2c0d820
Sazzad-Saju <sazzadsaju17@gmail.com>
2019-05-26 14:21:26 +0600

    sub corr: match replaced by null

--- arithmatic_calculation.cpp
+++ arithmatic_calculation.cpp
@@ -11,21 +11,21 @@ string to_str (int a){
 	out_string = ss.str();
 	return out_string;
 }
-void sub (string str1, string str2){
-	int i,j,k;
-	for(i=0; i<=str2.size() ; i++){   
-        for(j=0; j<=str1.size() ; j++ ){ 
-            if( str2[i] == str1[j] )
-            	//str1.erase(j);
-                str1[j] = NULL;
+string sub (string str1, string str2){
+	int i = 0, j = 0;
+    while (i < str1.size() && j < str2.size())
+    {
+        if (str1[i] == str2[j])
+        {
+            str1[i] = NULL;  // could use str1.erase(i,1)
+            i += 1;
+            j += 1;
         }
-	}
-	cout<<str1;
-/*
-	for(i=0; i <=str1.size()  ; i++){
-        if( str1[i] != '*' )
-           cout<<str1[i];
-	}*/
+        else
+            i += 1;
+    }
+    //cout<<str1;
+    return str1;
 }
 int main()
 {
@@ -40,18 +40,19 @@ int main()
 	cout<<setw(26)<<"Addition: "<<setw(13)<<a+b<<endl;
 	cout<<setw(30)<<"Substraction: "<<setw(9)<<a-b<<endl;
 	cout<<setw(32)<<"Multiplication: "<<setw(7)<<a*b<<endl;
-	cout<<setw(26)<<"Division: "<<setw(13)<<a/b<<endl<<endl; 
-	
+	cout<<setw(26)<<"Division: "<<setw(13)<<a/b<<endl<<endl;
+
 	cout<<setw(12)<<"String Calculation"<<endl;
 	cout<<setw(12)<<"--------------------------------"<<endl;
-	
+
 	string s1 = to_str(a);
 	string s2 = to_str(b);
 	//cout<< s1+s2;
 	cout<<setw(33)<<"String Addition: "<<setw(6)<<s1+s2<<endl;
 	//cout<<"Substraction: "<<s1-s2<<endl;
-	cout<<"Substraction: ";
-	sub(s1,s2);						//one drawback 122-2=1 not 12
-	
+	cout<<setw(30)<<"Substraction: "<<setw(8)<<sub(s1,s2)<<endl;
+	//sub(s1,s2);						//one drawback substraction contain null char
+
 	return 0;
 }
+
